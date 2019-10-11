23d2f78c8c22463a2967958ed39e3f9f43d40131
Tomasz Olędzki <luksfer@poczta.onet.pl>
2019-10-11 23:12:00 +0200

    Updates

---
+++ precache-manifest.93133d198259a36fd1c639989239ef76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be868cc737c21fb7459685e82ccdcfbe",
+    "url": "/star-wars-app/index.html"
+  },
+  {
+    "revision": "5378859cbfb92697b31c",
+    "url": "/star-wars-app/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "572273cbeeeb13f663b4",
+    "url": "/star-wars-app/static/css/main.ade4a97b.chunk.css"
+  },
+  {
+    "revision": "5378859cbfb92697b31c",
+    "url": "/star-wars-app/static/js/2.f89e79ac.chunk.js"
+  },
+  {
+    "revision": "572273cbeeeb13f663b4",
+    "url": "/star-wars-app/static/js/main.978856f2.chunk.js"
+  },
+  {
+    "revision": "4c32f8d470c2229d2b98",
+    "url": "/star-wars-app/static/js/runtime-main.0651a682.js"
+  }
+]);
